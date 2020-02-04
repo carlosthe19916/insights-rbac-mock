5c22ab6289ef2ffac7545731cd48cb412963deda
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-02-04 09:46:03 +0100

    add permissions as ENV

--- src/main/java/org/acme/config/AccessResource.java
+++ src/main/java/org/acme/config/AccessResource.java
@@ -2,6 +2,7 @@ package org.acme.config;
 
 import org.acme.models.Acl;
 import org.acme.models.RbacResponse;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.inject.Singleton;
 import javax.ws.rs.BadRequestException;
@@ -11,14 +12,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Path("/access")
 @Singleton
 public class AccessResource {
+
+    @ConfigProperty(name = "rbac.permissions")
+    String permissions;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -33,10 +37,10 @@ public class AccessResource {
             throw new BadRequestException("You need to send the x-rh-identity header");
         }
 
-        List<Acl> acls = new ArrayList<>();
-        acls.add(
-                new Acl("migration-analytics" + ":*:*", Collections.emptyList())
-        );
+        List<Acl> acls = Stream.of(permissions.split(","))
+                .map(String::trim)
+                .map(f -> new Acl(f, Collections.emptyList()))
+                .collect(Collectors.toList());
 
         RbacResponse.Meta meta = new RbacResponse.Meta(1, 10, 0);
         RbacResponse.Links links = new RbacResponse.Links(null, null, null, null);
